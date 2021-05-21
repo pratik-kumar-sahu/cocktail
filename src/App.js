1a1a7c12b9e02d4befa20b86280b795692a13731
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-21 20:22:24 +0530

    Building UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App">CockTail</div>;
+  return (
+    <div className="container">
+      <div className="leftContainer">
+        <form className="leftContainer__search">
+          <input
+            className="leftContainer__search--inputBox"
+            type="search"
+            placeholder="Search cocktails"
+          />
+          <button className="leftContainer__search--btn">Submit</button>
+        </form>
+      </div>
+      <div className="rightContainer"></div>
+    </div>
+  );
 }
 
 export default App;
